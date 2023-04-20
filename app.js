0752718ac763a10439cbe00a349b526c7dc465f3
theanmoldhanda <rk352714@gmail.com>
2023-04-20 20:13:11 +0530

    updated the new popup box

---
+++ app.js
@@ -0,0 +1,8 @@
+let popupbox = document.getElementById("popupbox")
+function openpopupbox() {
+    popupbox.classList.add("openpopup")
+}
+
+function closepopupbox() {
+    popupbox.classList.remove("openpopup")
+}
